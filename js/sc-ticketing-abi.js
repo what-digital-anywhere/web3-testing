084c406eee58a82252d36c1c3b59a3004233fb18
Mario Colombo <mario@what.digital>
2019-06-21 21:54:22 +0200

    events work now :O

--- js/sc-ticketing-abi.js
+++ js/sc-ticketing-abi.js
@@ -1,17 +1,3 @@
-// local ganache instance
-
-WS_ADDRESS = 'ws://0.0.0.0:7545';
-RCP_ADDRESS = 'http://0.0.0.0:7545';
-
-// RCP_ADDRESS = 'http://159.100.249.117:8545';
-// WS_ADDRESS = 'ws://159.100.249.117:8545';
-
-SC_TICKETNG_ADDRESS = "0xC45657951eFbbeeA2da3131Df796c0088e34439A";
-
-WALLET_ADDRESS = "0x70eD53133d3dddeC777D9E159C430cA7D3C9e1E3";
-WALLET_PRIVATE_KEY = '0x43ad9808d861dd1dc0ae09da94c3af96f3217fc41f3afee2acfa6bfda1f8dbb5';
-
-TRANSPORTER_ADDRESS = "0x7ED0e0b6DaF189af08a067852CcF45719f868203";
 
 SC_TICKETNG_ABI = [
     {
@@ -36,12 +22,6 @@ SC_TICKETNG_ABI = [
         "payable": false,
         "stateMutability": "view",
         "type": "function"
-    },
-    {
-        "inputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "constructor"
     },
     {
         "anonymous": false,
@@ -90,6 +70,11 @@ SC_TICKETNG_ABI = [
         "inputs": [
             {
                 "indexed": false,
+                "name": "tripIndex",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
                 "name": "startTimestamp",
                 "type": "uint256"
             },
@@ -113,6 +98,33 @@ SC_TICKETNG_ABI = [
         "type": "event"
     },
     {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "name": "tripIndex",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "name": "price",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "name": "transporterAddress",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "passengerAddress",
+                "type": "address"
+            }
+        ],
+        "name": "TripPriceSet",
+        "type": "event"
+    },
+    {
         "constant": false,
         "inputs": [
             {
@@ -124,6 +136,59 @@ SC_TICKETNG_ABI = [
         "outputs": [],
         "payable": false,
         "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "passengerAddress",
+                "type": "address"
+            },
+            {
+                "name": "tripIndex",
+                "type": "uint256"
+            }
+        ],
+        "name": "getTrips",
+        "outputs": [
+            {
+                "components": [
+                    {
+                        "name": "startTimestamp",
+                        "type": "uint256"
+                    },
+                    {
+                        "name": "endTimestamp",
+                        "type": "uint256"
+                    },
+                    {
+                        "name": "transporter",
+                        "type": "address"
+                    },
+                    {
+                        "name": "passenger",
+                        "type": "address"
+                    },
+                    {
+                        "name": "isCheckedOut",
+                        "type": "bool"
+                    },
+                    {
+                        "name": "isPaid",
+                        "type": "bool"
+                    },
+                    {
+                        "name": "price",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "",
+                "type": "tuple"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
         "type": "function"
     },
     {
@@ -183,5 +248,42 @@ SC_TICKETNG_ABI = [
         "payable": false,
         "stateMutability": "nonpayable",
         "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "passengerAddress",
+                "type": "address"
+            },
+            {
+                "name": "price",
+                "type": "uint256"
+            },
+            {
+                "name": "tripStartTimestamp",
+                "type": "uint256"
+            }
+        ],
+        "name": "setPrice",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "tripIndex",
+                "type": "uint256"
+            }
+        ],
+        "name": "payForTrip",
+        "outputs": [],
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "function"
     }
 ]
+
